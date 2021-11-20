7c13cbe59e663ea662cb8072d241f1c89d0feeed
ertgibson916 <ertgibson916@gmail.com>
2021-11-19 22:37:03 -0800

    added nav bar

--- RazorPagesAllegiant/Pages/Index.cshtml.cs
+++ RazorPagesAllegiant/Pages/Index.cshtml.cs
@@ -14,7 +14,11 @@ namespace RazorPagesAllegiant.Pages
 
         public void OnGet()
         {
-
         }
+
+
     }
+
+
 }
+
--- RazorPagesAllegiant/wwwroot/js/site.js
+++ RazorPagesAllegiant/wwwroot/js/site.js
@@ -1,4 +1,34 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+var sections = $('section')
+    , nav = $('nav')
+    , nav_height = nav.outerHeight();
 
+$(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function () {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+    });
+});
+
+nav.find('a').on('click', function () {
+    var $el = $(this)
+        , id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - nav_height
+    }, 500);
+
+    return false;
+});
 // Write your JavaScript code.
