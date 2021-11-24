5b570bbf64b22e99da955756f295582465ea3bb3
SomeJello <emarlette95@gmail.com>
2021-11-23 21:53:07 -0800

    Added fading animation for containers on scroll

--- RazorPagesAllegiant/wwwroot/js/site.js
+++ RazorPagesAllegiant/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {  // $(document).ready shorthand
+    $('.fadeInID').fadeIn('slow');
+});
+
+$(document).ready(function ()
+{
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+        var bottom_of_window = $(this).scrollTop() + $(this).innerHeight();
+        /* Check the location of each desired element */
+        $('.fadeIn').each(function (i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+            if ($(this).hasClass("timelineContainer")) 
+            {
+                if (bottom_of_window > bottom_of_object / 1.3) {
+
+                    $(this).animate({ 'opacity': '1' }, 1000);
+
+                }
+            }
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+                if ($(this).css("opacity") == 0) {
+                    $(this).animate({ 'opacity': '1' }, 1000);
+                }
+
+            }
+            else {
+                if ($(this).css("opacity") == 1) {
+                    $(this).animate({ 'opacity': '0' }, 1000);
+                }
+            }
+        });
+
+    }).scroll();
+
+});
