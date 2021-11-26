6ddcff168812fac7cda874a7aac195a8200bd4e8
Pix Mapanao <pmapanao@csus.edu>
2021-11-26 11:58:23 -0800

    commented out logger

--- RazorPagesAllegiant/Pages/Index.cshtml.cs
+++ RazorPagesAllegiant/Pages/Index.cshtml.cs
@@ -5,11 +5,11 @@ namespace RazorPagesAllegiant.Pages
     public class IndexModel : PageModel
     {
 
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
-            _logger = logger;
+            //_logger = logger;
         }
 
         public void OnGet()
